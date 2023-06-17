4f8871192e3738ae16a89da39e685747580b9c6b
MiimeSox <kontakt@malteschumacher.de>
2023-06-17 15:31:48 +0200

    Initial Commit

---
+++ SwiftuiMapApp/SwiftuiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapAppApp.swift
+//  SwiftuiMapApp
+//
+//  Created by Malte Schumacher on 17.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
